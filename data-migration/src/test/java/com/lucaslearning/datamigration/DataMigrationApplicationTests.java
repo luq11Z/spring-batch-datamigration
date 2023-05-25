bb308ae8cdd4a0f5913a35f67e0d2cfb8e2fbb72
Lucas Silva <lucas_segundinho@hotmail.com>
2023-05-25 19:59:19 +0100

    Project created

---
+++ data-migration/src/test/java/com/lucaslearning/datamigration/DataMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.datamigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
